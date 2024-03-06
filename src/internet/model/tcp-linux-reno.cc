4851aa02ee1e2e6c982e5c26090959d1788ccfe1
sanchit-1908 <sanchit.nits@gmail.com>
2024-03-06 19:23:39 +0530

    Update tcp-linux-reno.cc
    added 0.85 to cwnd

--- src/internet/model/tcp-linux-reno.cc
+++ src/internet/model/tcp-linux-reno.cc
@@ -144,6 +144,33 @@ TcpLinuxReno::GetSsThresh(Ptr<const TcpSocketState> state, uint32_t bytesInFligh
     return std::max<uint32_t>(2 * state->m_segmentSize, state->m_cWnd / 2);
 }
 
+TcpLinuxReno::EnterCwr(uint32_t currentDelivered)
+{
+    NS_LOG_FUNCTION(this << currentDelivered);
+    m_tcb->m_ssThresh = m_congestionControl->GetSsThresh(m_tcb, BytesInFlight());
+    m_tcb->m_ssThresh = (tcb->m_cWnd)*0.85;
+    NS_LOG_DEBUG("Reduce ssThresh to " << m_tcb->m_ssThresh);
+    // Do not update m_cWnd, under assumption that recovery process will
+    // gradually bring it down to m_ssThresh.  Update the 'inflated' value of
+    // cWnd used for tracing, however.
+    m_tcb->m_cWndInfl = m_tcb->m_ssThresh;
+    NS_ASSERT(m_tcb->m_congState != TcpSocketState::CA_CWR);
+    NS_LOG_DEBUG(TcpSocketState::TcpCongStateName[m_tcb->m_congState] << " -> CA_CWR");
+    m_tcb->m_congState = TcpSocketState::CA_CWR;
+    // CWR state will be exited when the ack exceeds the m_recover variable.
+    // Do not set m_recoverActive (which applies to a loss-based recovery)
+    // m_recover corresponds to Linux tp->high_seq
+    m_recover = m_tcb->m_highTxMark;
+    if (!m_congestionControl->HasCongControl())
+    {
+        // If there is a recovery algorithm, invoke it.
+        m_recoveryOps->EnterRecovery(m_tcb, m_dupAckCount, UnAckDataCount(), currentDelivered);
+        NS_LOG_INFO("Enter CWR recovery mode; set cwnd to " << m_tcb->m_cWnd << ", ssthresh to "
+                                                            << m_tcb->m_ssThresh << ", recover to "
+                                                            << m_recover);
+    }
+}
+
 Ptr<TcpCongestionOps>
 TcpLinuxReno::Fork()
 {
